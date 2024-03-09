49478187e9fd07c6bdbe94628da1fa2c5d8f9919
Raúl Villares <raul@devengo.com>
2024-03-09 19:41:13 +0100

    Adding a new blog post action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  root "blog_posts#index"
 end
